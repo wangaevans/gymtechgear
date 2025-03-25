df7a58ec23dad2bc85556fe1fa24bd9e118ee49e
Wanga Evans <85017101+wangaevans@users.noreply.github.com>
2025-03-25 13:31:39 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import Header from "./components/header";
 import { Toaster } from 'react-hot-toast';
 import ClientProviders from "./components/ClientProviders";
 import { Analytics } from "@vercel/analytics/react"
-import Script from 'next/script'
+import { GoogleAdSense } from "next-google-adsense";
 
 export default async function RootLayout({
   children,
@@ -18,17 +18,12 @@ export default async function RootLayout({
       <html lang="en">
         <body>
           <Analytics/>
+            <GoogleAdSense publisherId="pub--8281092193891669" /> 
           <Header />
           <Toaster position="top-right" toastOptions={{ duration: 5000 }} />
           {children}
           <ClientProviders/>
           <Footer />
-       <Script 
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8281092193891669"
-          crossOrigin="anonymous"
-          strategy="afterInteractive"
-        />
         </body>
       </html>
       
