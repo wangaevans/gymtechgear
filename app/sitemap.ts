a33c3c8c357a706c238dec18791269129f43e096
Wanga Evans <85017101+wangaevans@users.noreply.github.com>
2025-03-28 08:10:20 +0300

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -9,19 +9,31 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1,
     },
     {
+      url: "https://gymtechgear.com/ads.txt",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
+    },
+    {
+      url: "https://gymtechgear.com/privacy-policy",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
+    },
+    {
       url: "https://gymtechgear.com/about",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.8,
     },
     {
-      url: "https://gymtechgear.com/collections/men",
+      url: "https://gymtechgear.com/audience/men",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.5,
     },
     {
-      url: "https://gymtechgear.com/collections/women",
+      url: "https://gymtechgear.com/audience/women",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.5,
