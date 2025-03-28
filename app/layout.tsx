98ebbb98b7bcc3fa9fdb94d79a19f9e4753eb6db
Wanga Evans <85017101+wangaevans@users.noreply.github.com>
2025-03-28 08:18:08 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,30 +5,33 @@ import { Toaster } from 'react-hot-toast';
 import ClientProviders from "./components/ClientProviders";
 import { Analytics } from "@vercel/analytics/react"
 import { GoogleAdSense } from "next-google-adsense";
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  robots: {
+    index: true,
+    follow: true,
+  },
+  title: 'Gymtechgear',
+  description: 'Innovative Fitness Solutions',
+};
 
 export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  // Fetch categories at build time
-  // const categories = await client.fetch(allCategoriesQuery);
-
   return (
-      <html lang="en">
-        <head>
-          <meta name="robots" content="index, follow">
-        </head>
-        <body>
-          <Analytics/>
-            <GoogleAdSense publisherId="pub-8281092193891669" /> 
-          <Header />
-          <Toaster position="top-right" toastOptions={{ duration: 5000 }} />
-          {children}
-          <ClientProviders/>
-          <Footer />
-        </body>
-      </html>
-      
+    <html lang="en">
+      <body>
+        <Analytics />
+        <GoogleAdSense publisherId="pub-8281092193891669" /> 
+        <Header />
+        <Toaster position="top-right" toastOptions={{ duration: 5000 }} />
+        {children}
+        <ClientProviders />
+        <Footer />
+      </body>
+    </html>
   );
 }
